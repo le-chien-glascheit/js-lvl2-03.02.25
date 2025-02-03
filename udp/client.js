144e2531732d81c727830ae2dc276848b511bc39
student <student@local.dev>
2025-02-03 12:46:07 +0300

    udp client

---
+++ udp/client.js
@@ -0,0 +1,11 @@
+const dgram = require('dgram');
+
+const socket = new dgram.Socket('udp4'); // UDP, IPv4
+
+socket.on('message', (msg, rinfo) => {
+    console.log(msg, rinfo);
+});
+
+socket.send('request\n', 9999, '127.0.0.1', (err) => {
+    console.log('sent');
+});
