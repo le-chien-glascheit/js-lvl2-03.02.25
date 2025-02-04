4f4f1ab5a0ee11ea2b9e5e5a620d8c69ad233fae
student <student@local.dev>
2025-02-04 11:16:43 +0300

    http server

--- Readme.md
+++ Readme.md
@@ -26,4 +26,27 @@ module.exports = {
 }
 ```
 
-## Protocol TCP
+## Protocol HTTP
+
+RFC ->
+
+http://ya.ru:80/search?q=js
+
+1. Request Line: GET|POST|DELETE|... part_of_url /search?q=js HTTP/1.1\r\n
+2. Headers: Key: Value
+2.0. Host: ya.ru:80 
+2.1. Accept: image/png\r\n
+2.2. Content-Length: length in bytes\r\n (Transfer-Encoding)
+\r\n
+3. (opt) Body: bytes
+
+1. Status Line: HTTP/1.1 200 OK\r\n
+2. Headers: Key: Value
+2.0. Host: ya.ru:80 
+2.1. Accept: image/png\r\n
+2.2. Content-Length: length in bytes\r\n (Transfer-Encoding)
+\r\n
+3. (opt) Body: bytes
+
+1. RL + Headers | SL + Headers
+2. Body
---
+++ http/server.js
@@ -0,0 +1,31 @@
+import {Server} from 'node:http';
+
+const server = new Server();
+server.keepAliveTimeout = 30_000;
+
+server.on('connection', () => {
+    console.log('someone connected');
+});
+
+server.on('request', (req, res) => {
+    // status line &
+    // headers already parsed
+    console.log(req);
+
+    // body
+    const bodyChunks = [];
+    req.on('data', (chunk) => {
+        bodyChunks.push(chunk);
+    });
+    req.on('end', () => { // body end, not socket
+        const body = Buffer.concat(bodyChunks).toString('utf8');
+        console.log(body);
+        // no Content-Length: , Transfer-Encoding: hex
+        res.writeHead(200, {
+            'content-type': 'application/json',
+        });
+        res.end('{"status": "ok"}');
+    });
+});
+
+server.listen(9999);
