b5fcb4ce038b0eb04fce98b7e2fc8d743819aab2
student <student@local.dev>
2025-02-05 12:32:51 +0300

    audio practice

--- project/jsnetwork.mjs
+++ project/jsnetwork.mjs
@@ -1,1 +1,16 @@
 console.log('js network');
+
+// Resource - img, audio, video
+// document.createElement
+// 
+const imgEl = new Image();
+imgEl.src = 'http://localhost:9999/static/js.png';
+
+setTimeout(() => {
+    // 1. new Audio()
+    // 2. src
+    // 3. play
+}, 5000);
+
+// Link
+// Form
